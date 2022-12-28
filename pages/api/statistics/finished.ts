fbb5553c6a6a800a9cc70b233e437659a1abcd44
Malte2036 <malte2036@gmail.com>
2022-12-28 22:51:05 +0100

    :heavy_plus_sign: use appwrite instead of mysql

--- pages/api/statistics/finished.ts
+++ pages/api/statistics/finished.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+
+import sdk from "node-appwrite";
 
 export type StatisticsData = {
   startDruck: number;
@@ -11,10 +12,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const body = req.body as StatisticsData;
 
-    const prisma = new PrismaClient();
-    await prisma.statistics.create({
-      data: body,
-    });
+    let client = new sdk.Client();
+    const databases = new sdk.Databases(client);
+
+    client
+      .setEndpoint(process.env.APPWRITE_ENDPOINT ?? "")
+      .setProject(process.env.APPWRITE_PROJECTID ?? "")
+      .setKey(process.env.APPWRITE_APIKEY ?? "");
+
+    await databases.createDocument(
+      process.env.APPWRITE_DATABASEID ?? "",
+      process.env.APPWRITE_COLLECTIONID_STATISTICS ?? "",
+      "unique()",
+      body
+    );
 
     res.status(200).end();
   }
