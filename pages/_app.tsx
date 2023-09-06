ef7fe915b35c88e53fe840538427201443558051
Malte2036 <malte2036@gmail.com>
2023-09-06 22:07:58 +0200

    :chart_with_upwards_trend: Add umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,11 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <script
+            async
+            src={process.env.NEXT_PUBLIC_UMAMI_ENDPOINT}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITEID}
+          ></script>
         </Head>
         <body>
           <Main />
